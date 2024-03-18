7d069069d83a01c0f4d88be3d5268bca1b2790b0
StefSalv07 <202312023@daiict.ac.in>
2024-03-18 22:05:41 +0530

    server+mongodb Connect

--- Sathi-Hub-5dw/backend/index.js
+++ Sathi-Hub-5dw/backend/index.js
@@ -1,2 +1,20 @@
-const express = require('express')// creating the server
-const mongoose = require('mongoose')// connecting to database 
+const express = require("express"); // creating the server
+const app = express();
+const mongoose = require("mongoose"); // connecting to database
+// const axios = require('axios')// calling the services
+// <7-? callback
+mongoose
+  .connect("mongodb://127.0.0.1:27017/production")
+  .then((sucess) => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen(3000, (err, success) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server is running on port=>" + 3000);
+  }
+}); //where my services will called()
